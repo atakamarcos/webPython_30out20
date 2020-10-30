3a74bbd87b6cdc95e8caae5f25cf3aad3e993274
atakamarcos <atakamarcos@gmail.com>
2020-10-30 19:40:44 +0000

    falta apenas ver variavel

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -6,10 +6,20 @@ app = Flask(__name__)
 @app.route('/')
 
 def index():
-    return "!!New ** Hello *** World!!"
+    return render_template('index.html')
 
 
 @app.route('/marcos')
 
 def marcos():
     return "!!***New  Hello Marcos Ataka***!!"
+
+@app.route('/<nome>')
+
+def ola_com_nome(nome):
+    return render_template('index.html', nome_pessoa = nome)
+
+# def ola_com_nome(nome):
+#    return "Hello {nome} !" como funciona isso aqui, variavel??
+
+#@app.route('/<nome>')
