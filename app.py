68665a14be4adb610f16e3286ef3df0249243951
atakamarcos <atakamarcos@gmail.com>
2020-10-30 19:06:46 +0000

    com o env setado no flask

--- app.py
+++ app.py
@@ -6,5 +6,10 @@ app = Flask(__name__)
 @app.route('/')
 
 def index():
-    return "!!New ** Hello ** World!!"
+    return "!!New ** Hello *** World!!"
 
+
+@app.route('/marcos')
+
+def marcos():
+    return "!!***New  Hello Marcos Ataka***!!"
